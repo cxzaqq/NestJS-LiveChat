10a49dd94729b070f9eac83346213560c00be7ac
cxzaqq <roy1228@naver.com>
2023-06-21 18:05:13 +0900

    reset

--- hi-nest/src/app.module.ts
+++ hi-nest/src/app.module.ts
@@ -2,10 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
-import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -19,12 +16,10 @@ import { AuthModule } from './auth/auth.module';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PW,
       database: process.env.DB_NAME,
-      entities: [User],
       synchronize: true,
       autoLoadEntities: true,
+      logging: true,
     }),
-    UsersModule,
-    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- hi-nest/src/auth/dto/sign-in.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsString } from 'class-validator';
-
-export class SignInDto {
-  @IsString()
-  userId: string;
-
-  @IsString()
-  password: string;
-}
