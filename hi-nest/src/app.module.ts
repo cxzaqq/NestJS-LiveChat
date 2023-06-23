e95f213b7c1d0ec1ced457616d281744fdfcc8fb
cxzaqq <roy1228@naver.com>
2023-06-23 16:44:27 +0900

    Authentication

--- hi-nest/src/app.module.ts
+++ hi-nest/src/app.module.ts
@@ -3,6 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -20,6 +22,8 @@ import { ConfigModule } from '@nestjs/config';
       autoLoadEntities: true,
       logging: true,
     }),
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
