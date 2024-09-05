d44185925e06d65c3da892394359237440ba5571
cxzaqq <roy1228@naver.com>
2024-09-05 19:59:40 +0900

    add unit test

---
+++ src/auth/auth.service.spec.ts
@@ -0,0 +1,80 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { UnauthorizedException } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+
+describe('AuthService', () => {
+  let authService: AuthService;
+  let userService: UserService;
+  let jwtService: JwtService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UserService,
+          useValue: {
+            findOne: jest.fn(),
+          },
+        },
+        {
+          provide: JwtService,
+          useValue: {
+            signAsync: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    authService = module.get<AuthService>(AuthService);
+    userService = module.get<UserService>(UserService);
+    jwtService = module.get<JwtService>(JwtService);
+  });
+
+  const mockUser = {
+    id: '1',
+    userId: 'validUserId',
+    userPw: 'correctPassword',
+    userNickname: 'nickname',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+
+  it('should be defined', () => {
+    expect(authService).toBeDefined();
+    expect(userService).toBeDefined();
+    expect(jwtService).toBeDefined();
+  });
+
+  it('should throw an error if user is not found', async () => {
+    jest.spyOn(userService, 'findOne').mockResolvedValue(null);
+
+    await expect(
+      authService.signIn('invalidUserId', 'password'),
+    ).rejects.toThrow(new UnauthorizedException('user not found'));
+  });
+
+  it('should throw an error if password is incorrect', async () => {
+    jest.spyOn(userService, 'findOne').mockResolvedValue(mockUser);
+
+    await expect(
+      authService.signIn('validUserId', 'wrongPassword'),
+    ).rejects.toThrow(new UnauthorizedException('Invalid password'));
+  });
+
+  it('should return an JWT token if credentails are valid', async () => {
+    const mockToken = 'mockJwtToken';
+
+    jest.spyOn(userService, 'findOne').mockResolvedValue(mockUser);
+    jest.spyOn(jwtService, 'signAsync').mockResolvedValue(mockToken);
+
+    const result = await authService.signIn('validUserId', 'correctPassword');
+
+    expect(result).toEqual({
+      access_token: mockToken,
+      statusCode: 200,
+    });
+  });
+});
--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,18 +1,55 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 describe('UserService', () => {
-  let service: UserService;
+  let userService: UserService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: PrismaService,
+          useValue: {
+            user: {
+              findUnique: jest.fn(),
+            },
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<UserService>(UserService);
+    userService = module.get<UserService>(UserService);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
+  const mockUser = {
+    id: '1',
+    userId: 'validUserId',
+    userPw: 'correctPassword',
+    userNickname: 'nickname',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(userService).toBeDefined();
+    expect(prismaService).toBeDefined();
+  });
+
+  it('should return a user if found', async () => {
+    jest.spyOn(prismaService.user, 'findUnique').mockResolvedValue(mockUser);
+
+    const user = await userService.findOne('validUserId');
+    expect(user).toEqual(mockUser);
+  });
+
+  it('should return null if no user is found', async () => {
+    jest.spyOn(prismaService.user, 'findUnique').mockResolvedValue(null);
+
+    const user = await userService.findOne('none');
+    expect(user).toBeNull();
   });
 });
